51369449af4f305263b5160ced73b7963e412961
TheBlackBloodyUnicorn <TheBlackBloodyUnicorn@hotmail.com>
2015-11-23 14:21:08 +0000

    new test

--- retrieve.js
+++ retrieve.js
@@ -31,15 +31,15 @@ http.createServer(function(request, response) {
             var results = collection.find({name: 'modulus user'});
 
             results.each(function (err, result) {
-                if (err) {
-                    response.write(err);
-                } else {
-                    response.write('Fetched: ' + result.name + " : " + result.age + " : " + result.roles.toString() +'\n');
-                }
-                //if the result is null, there are no more results, it’s ok to close everything
                 if (result == null) {
                     response.end('Completed');
                     db.close();
+                }else {
+                    if (err) {
+                        response.write(err);
+                    } else {
+                        response.write('Fetched: ' + result.name + " : " + result.age + " : " + result.roles.toString() + '\n');
+                    }
                 }
             });
 
