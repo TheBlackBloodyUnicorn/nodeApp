8614665b35d50fabb4845ede2603ff78386bc3f7
TheBlackBloodyUnicorn <TheBlackBloodyUnicorn@hotmail.com>
2015-11-23 13:21:30 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 1508593 on 23/11/2015.
+ */
+var http = require('http')
+var port = process.env.PORT || 1337;
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}).listen(port);
+
